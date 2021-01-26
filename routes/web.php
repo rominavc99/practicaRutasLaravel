1c5f21c4788b152bd788a4ef6edaec6141ea1d43
rominavc99 <procrom1999@gmail.com>
2021-01-26 10:10:18 -0700

    practica rutas

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('notas/{id}/detalles', function ($id) {
+    return 'Aqui se muestran los detalles de la nota: ' .$id;
+});
+
+Route::get('notas/crear', function () {
+    return 'aqui se crea una nueva nota';
+});
+
+Route::get('notas/listar', function () {
+    return 'aqui se listan las notas';
+});
+
+Route::get('notas/editar', function () {
+    return 'aqui se editan las notas';
+});
+
